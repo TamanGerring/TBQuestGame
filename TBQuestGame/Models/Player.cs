32a9bc5beff114113f1fffa3f0ef70d416b73d35
TamanGerring <taman.gerring@yahoo.com>
2015-11-05 22:00:00 -0500

    stuff

--- TBQuestGame/Models/Player.cs
+++ TBQuestGame/Models/Player.cs
@@ -8,6 +8,18 @@ namespace TBQuestGame
 {
     public class Player : Models.Character
     {
-        
+        public enum ActionChoice
+        {
+            None,
+            QuitGame,
+            Move
+        }
+
+        private int _actionCount = Enum.GetNames(typeof(ActionChoice)).Length;
+
+        public int ActionCount
+        {
+            get { return _actionCount; }
+        }
     }
 }
